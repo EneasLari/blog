768646d7966d449277b5972b652dfaa462952ce6
EneasLari <cs02367@cc.uoi.gr>
2020-12-28 23:53:48 +0200

    Update postloading.js

--- js/postloading.js
+++ js/postloading.js
@@ -1,7 +1,7 @@
-var titlesection=document.getElementById("postTitle")
-var descriptionsection=document.getElementById("postDescription")
-var bodysection=document.getElementById("postBody")
-var id=document.getElementById("ArticleId")
+var titlesection = document.getElementById("postTitle")
+var descriptionsection = document.getElementById("postDescription")
+var bodysection = document.getElementById("postBody")
+var id = document.getElementById("ArticleId")
 
 console.log(titlesection);
 console.log(descriptionsection);
@@ -9,24 +9,24 @@ console.log(bodysection);
 console.log(id.innerHTML);
 
 window.onload = function () {
-
+    getPost();
 }
 
 function getPost() {
     // Make a request for a user with a given ID
     commentssection.innerHTML = "";
     axios.get('https://articlecommentsapi.herokuapp.com/articlebyid', {
-            params: {
-                id: id.innerHTML
-            }
-        })
+        params: {
+            id: id.innerHTML
+        }
+    })
         .then(function (response) {
             // handle success
             console.log(response.data);
-            titlesection.innerHTML=response.data.Title;
-            descriptionsection.innerHTML=response.data.Description;
-            bodysection.innerHTML=response.data.Body;
-            
+            titlesection.innerHTML = response.data.Title;
+            descriptionsection.innerHTML = response.data.Description;
+            bodysection.innerHTML = response.data.Body;
+
         })
         .catch(function (error) {
             // handle error
