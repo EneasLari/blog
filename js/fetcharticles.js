a5df29300c80d31c016562559245d2a112c3797b
EneasLari <cs02367@cc.uoi.gr>
2020-12-28 19:50:14 +0200

    Update fetcharticles.js

--- js/fetcharticles.js
+++ js/fetcharticles.js
@@ -1,6 +1,8 @@
 var articlescontainer=document.getElementById("dynamiccontent")
 getArticles();
 
+
+
 function getArticles() {
     // Make a request for a user with a given ID
     articlescontainer.innerHTML = "";
@@ -10,12 +12,15 @@ function getArticles() {
             // }
         })
         .then(function (response) {
-            articlescontainer.innerHTML=response.data;
+            
             // handle success
             var i;
+            var articlepreview='';
             for (i = response.data.length - 1; i >= 0; i--) {
+                articlepreview=articlepreview+'<div class="row"><div class="col-lg-8 col-md-10 mx-auto"><div class="post-preview"><a href="posts/post5.html"><h2 class="post-title">'+response.data[i].Title+'</h2><h3 class="post-subtitle">'+response.data[i].Body+'</h3></a> <p class="post-meta">Posted by<a href="https://twitter.com/EneasLari">Eneas Lari</a>on December 8, 2020</p></div><hr></div></div>';
                 console.log(response.data[i])
             }
+            articlescontainer.innerHTML=rarticlepreview;
         })
         .catch(function (error) {
             // handle error
